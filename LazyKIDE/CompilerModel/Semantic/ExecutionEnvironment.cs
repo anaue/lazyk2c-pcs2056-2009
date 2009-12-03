eb9aedab50ecbf017dacdc245fea7eb6dedcbcc6
anaue.costa@gmail.com <anaue.costa@gmail.com@3cd1ef08-e048-11de-ae3c-8df03fb1c7b4>
2009-12-03 23:17:20 +0000

    get execution environment

---
+++ LazyKIDE/CompilerModel/Semantic/ExecutionEnvironment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CompilerModel.Semantic
+{
+    class ExecutionEnvironment
+    {
+        private StreamReader _reader;
+        private String _pathName;
+        private String _definitions;
+
+        public ExecutionEnvironment(string path)
+        {
+            _pathName = path;
+        }
+
+        public ExecutionEnvironment()
+        {
+            _pathName = "ExecutionEnvironment//environment.c";
+        }
+        public void GetExecutionEnvFromFile()
+        {
+            string directoryName = Path.GetDirectoryName(_pathName);
+            if (!Directory.Exists(directoryName))
+                Directory.CreateDirectory(directoryName);
+            _reader = new StreamReader(_pathName, Encoding.Default,false);
+            _definitions = _reader.ReadToEnd();
+            _reader.Close();
+        }
+        public String GetDefinitions()
+        {
+            return _definitions;
+        }
+    }
+}
